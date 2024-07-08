2810008cf1605bbc1371e39f80a24fce46194bae
Runlong (Mike) He <42667715+HRL-Mike@users.noreply.github.com>
2024-07-08 19:36:54 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,13 @@
+'''
+Description: Model implementation of PitVQA-Net model
+Paper: PitVQA: Image-grounded Text Embedding LLM for Visual Question Answering in Pituitary Surgery
+Author: Runlong He, Mengya Xu, Adrito Das, Danyal Z. Khan, Sophia Bano, Hani J. Marcus, Danail Stoyanov, Matthew J. Clarkson, Mobarakol Islam
+Lab: Wellcome/EPSRC Centre for Interventional and Surgical Sciences (WEISS), UCL
+Acknowledgement : Code adopted from the official implementation of 
+                  BLIP (https://huggingface.co/docs/transformers/model_doc/blip) and
+                  Surgical-GPT (https://github.com/lalithjets/SurgicalGPT).
+'''
+
 import torch
 from torch import nn
 import torch.nn.functional as F
